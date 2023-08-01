71ba641841a12d653c6e50163ae63507e6f62879
1100810 Daniel Campos <1100810@isep.ipp.pt>
2023-08-01 20:13:28 +0100

    circle area

---
+++ CircleArea/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    const double pi = 3.14;
+
+    cout << "Enter radius: ";
+    double radius;
+    cin >> radius;
+
+    double area = pi * pow(radius, 2);
+
+    cout << "Area: " << area;
+
+    return 0;
+}
